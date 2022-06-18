fefc0668e38df8f620daec4d5ac1bfed1a6634df
Masataka SUZUKI <labduck@gmail.com>
2022-06-18 09:17:14 +0900

    todo追加機能を作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // li生成
+  const li = document.createElement("li");
+  li.textContent = inputText;
+  // divにliを差し込む
+  div.appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
+  // ボタンの生成
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "完了";
+  completeButton.addEventListener("click", () => {
+    console.log("完了");
+  });
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "削除";
+  deleteButton.addEventListener("click", () => {
+    console.log("削除");
+  });
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
